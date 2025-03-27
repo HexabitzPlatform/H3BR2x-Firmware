fe15a89ca7a6ea2f054aa9ea9bdf687951e3df5d
Mahmood_Reda <mr4141705@gmail.com>
2025-03-27 17:53:30 +0300

    add ((GetModuleParameter)) empty function

--- H3BR2/H3BR2.c
+++ H3BR2/H3BR2.c
@@ -35,7 +35,7 @@ Segment_Codes Digit[2] = {Empty,Empty}; //Digit[0]: LSD, Digit[1]: MSD
  uint8_t StartSevSeg;
 
 /* Module exported parameters ------------------------------------------------*/
-module_param_t modParam[NUM_MODULE_PARAMS] ={{.paramPtr = NULL, .paramFormat =FMT_FLOAT, .paramName =""}};
+ module_param_t modParam[NUM_MODULE_PARAMS] ={0};
 
 /* Private variables ---------------------------------------------------------*/
 uint8_t index_7_seg = 0;
@@ -113,6 +113,27 @@ const CLI_Command_Definition_t CLI_SevenDisplayOneDigitHexaCommandDefinition =
  |						 Private Functions	 														|
  -----------------------------------------------------------------------
  */
+/***************************************************************************/
+/* This functions is useful only for input (sensors) modules.
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex,float *value){
+	Module_Status status =BOS_OK;
+
+	switch(paramIndex){
+
+		/* Invalid parameter index */
+		default:
+			status =BOS_ERR_WrongParam;
+			break;
+	}
+
+	return status;
+}
+
 /*-----------------------------------------------------------*/
 Segment_Codes get_number_code(uint8_t digit)
 {
